60b2121a4c175dc579033ff4cbbdca43fedc0a7c
scorpion19-wist <wistlandsagbo@gmail.com>
2025-11-12 16:50:14 +0100

    Refactor competence.php to improve SQL query syntax and enhance HTML structure for displaying skills

--- competence.php
+++ competence.php
@@ -9,9 +9,21 @@ include "INC/composants/header.php";
 <?php
 include("INC/connexions.php");
 
-$sql_requete = "SELECT * FROM competences";
+$sql_requete = 'SELECT * FROM competences;';
 $resultat = $connexion->query($sql_requete);
 $competences = $resultat->fetchAll();
 
 ?>
 
+<ul class="list-disc pl-6 space-y-2">
+    <?php foreach ($competences as $c){?>
+        <li>
+            <?php echo $c['nom']; ?> <br>
+            <?php echo $c['niveau']; ?>
+        </li>
+    <?php } ?>
+</ul>
+
+<?php
+include "INC/composants/footer.php";
+?>  
